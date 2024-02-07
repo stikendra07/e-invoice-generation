53cf69c1bb69999bade20761e2e3a72b94791c44
stikendra <97972178+stikendra07@users.noreply.github.com>
2024-02-07 20:19:44 +0530

    Add files via upload

---
+++ css/assets/js/pages/form-editor.init.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$("#summernote-editor").summernote({height:250,minHeight:null,maxHeight:null,focus:!1}),$("#summernote-inline").summernote({airMode:!0})
+
+	$("#summernote-editor2").summernote({height:150,minHeight:null,maxHeight:null,focus:!1}),$("#summernote-inline").summernote({airMode:!0})
+});
